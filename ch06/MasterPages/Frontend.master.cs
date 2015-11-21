0ef698126598a63a1b12111c381f308767abc537
lizhen325 <zhenli336@gmail.com>
2015-11-22 00:02:50 +1300

    new file:   ch06/App_Code/BasePage.cs 	new file:   ch06/App_Code/Calculator.cs 	new file:   ch06/App_Themes/DarkGrey/DarkGrey.css 	new file:   ch06/App_Themes/DarkGrey/Images/Header.jpg 	new file:   ch06/App_Themes/Monochrome/Button.skin 	new file:   ch06/App_Themes/Monochrome/Images/Header.jpg 	new file:   ch06/App_Themes/Monochrome/Images/MenuBackground.jpg 	new file:   ch06/App_Themes/Monochrome/Images/Sidebar.jpg 	new file:   ch06/App_Themes/Monochrome/Monochrome.css 	new file:   ch06/Default.aspx 	new file:   ch06/Default.aspx.cs 	new file:   ch06/Demos/CalculatorDemo.aspx 	new file:   ch06/Demos/CalculatorDemo.aspx.cs 	new file:   ch06/Demos/CodeBehind.aspx 	new file:   ch06/Demos/CodeBehind.aspx.cs 	new file:   ch06/Demos/CodeInline.aspx 	new file:   ch06/Demos/Containers.aspx 	new file:   ch06/Demos/Containers.aspx.cs 	new file:   ch06/Demos/ControlsDemo.aspx 	new file:   ch06/Demos/ControlsDemo.aspx.cs 	new file:   ch06/Demos/CssDemo.aspx 	new file:   ch06/Demos/CssDemo.aspx.cs 	new file:   ch06/Demos/ListControls.aspx 	new file:   ch06/Demos/ListControls.aspx.cs 	new file:   ch06/Demos/SkinsDemo.aspx 	new file:   ch06/Demos/SkinsDemo.aspx.cs 	new file:   ch06/Demos/State.aspx 	new file:   ch06/Demos/State.aspx.cs 	new file:   ch06/Demos/TableDemo.aspx 	new file:   ch06/Demos/TableDemo.aspx.cs 	new file:   ch06/Login.aspx 	new file:   ch06/Login.aspx.cs 	new file:   ch06/MasterPages/Frontend.master 	new file:   ch06/MasterPages/Frontend.master.cs 	new file:   ch06/Scripts/modernizr-2.7.1.js 	new file:   ch06/Web.Debug.config 	new file:   ch06/Web.config 	new file:   ch06/packages.config

---
+++ ch06/MasterPages/Frontend.master.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPages_Frontend : System.Web.UI.MasterPage
+{
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    if (!Page.IsPostBack)
+    {
+      string selectedTheme = Page.Theme;
+      HttpCookie preferredTheme = Request.Cookies.Get("PreferredTheme");
+      if (preferredTheme != null)
+      {
+        selectedTheme = preferredTheme.Value;
+      }
+      if (!string.IsNullOrEmpty(selectedTheme))
+      {
+        ListItem item = ThemeList.Items.FindByValue(selectedTheme);
+        if (item != null)
+        {
+          item.Selected = true;
+        }
+      }
+    }
+  }
+
+  protected void ThemeList_SelectedIndexChanged(object sender, EventArgs e)
+  {
+    HttpCookie preferredTheme = new HttpCookie("PreferredTheme");
+    preferredTheme.Expires = DateTime.Now.AddMonths(3);
+    preferredTheme.Value = ThemeList.SelectedValue;
+    Response.Cookies.Add(preferredTheme);
+    Response.Redirect(Request.Url.ToString());
+  }
+}
